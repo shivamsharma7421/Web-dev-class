2703c68e4b7e534f81bee5a0613efc9929473404
Your Name <you@example.com>
2024-12-16 10:15:12 +0530

    JavaScript Data Types

--- logic.js
+++ logic.js
@@ -1,3 +1,25 @@
 const price1 = 5;
 const price2 = 10;
 let total= price1 + price2;
+
+//number
+let length = 54;
+
+// string
+let color = "yellow";
+
+//boolean
+let x = true;
+let y = false;
+
+// object
+const person = {
+
+    name: "spider",
+    age:  18,
+}
+
+// array object
+const cars =[audi, bmw, toyota, mazda];
+
+const date = new Date("20024-12-11")
